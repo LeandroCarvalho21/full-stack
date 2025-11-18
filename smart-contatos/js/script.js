ba9cc7686283b3ebfe19c4f6a4fbe67e63e72b78
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-11-18 15:03:28 -0300

    Adicionando função para enviar o formulario e um if para validar

--- smart-contatos/js/script.js
+++ smart-contatos/js/script.js
@@ -1,17 +1,18 @@
+const form = document.querySelector("form");
 const inputName = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
 const inputTel = document.getElementById("telefone");
 
-
-function cadastro(event) {
+form.addEventListener("submit", function (event) {
     event.preventDefault();
-    // let li = createElement("li");
-    
-
-    // exibi no console
+    //Validação do formulario
+    if (inputName.value == "" || inputEmail.value == "" || inputTel.value == "") {
+        alert("Digite seus dados");
+        return false
+    }
+    alert("Dados cadastrados");
     console.log(inputName.value);
     console.log(inputEmail.value);
     console.log(inputTel.value);
-    console.log(event);
-    
-}
+
+})
