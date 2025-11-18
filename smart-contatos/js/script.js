bcf6a2ac53e161a2121a17cb5503122afe3cfa23
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-11-18 16:19:22 -0300

    Adicionando conteudo na estrutura html com java script

--- smart-contatos/js/script.js
+++ smart-contatos/js/script.js
@@ -1,18 +1,30 @@
 const form = document.querySelector("form");
+const lista = document.querySelector(".lista");
 const inputName = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
 const inputTel = document.getElementById("telefone");
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
+
     //Validação do formulario
     if (inputName.value == "" || inputEmail.value == "" || inputTel.value == "") {
-        alert("Digite seus dados");
+        alert("Digite seus dados")
         return false
     }
-    alert("Dados cadastrados");
-    console.log(inputName.value);
-    console.log(inputEmail.value);
-    console.log(inputTel.value);
+
+    //Criar li com javascript
+    const li = document.createElement("li");
+    li.innerHTML = `
+    <span class = "contato-nome">${inputName.value}</span> 
+    <span class = "contato-nome">${inputEmail.value}</span>
+    <span class = "contato-nome">${inputTel.value}</span>
+    `;
+
+    //Adiciona um o elemento li dentro do ul na lista html
+    lista.appendChild(li);
+
+    //resetar o formulario
+    form.reset();
 
 })
