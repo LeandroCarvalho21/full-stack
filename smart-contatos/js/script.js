3932cbca62f91c3e60a2240d967ac0f17bad619a
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-11-19 14:42:45 -0300

    removendo a li do HTML com javaScript

--- smart-contatos/js/script.js
+++ smart-contatos/js/script.js
@@ -1,30 +1,42 @@
 const form = document.querySelector("form");
+const listaMsg = document.querySelector("#listaMsg")
 const lista = document.querySelector(".lista");
 const inputName = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
 const inputTel = document.getElementById("telefone");
+
+
+function criarBtn(){
+    
+}
+
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
 
     //Validação do formulario
     if (inputName.value == "" || inputEmail.value == "" || inputTel.value == "") {
-        alert("Digite seus dados")
+        alert("Digite seus dados");
         return false
+    } else if (inputName.value != "" && inputEmail.value != "" && inputTel.value != "") {
+        //Criar li com javascript
+        const li = document.createElement("li");
+        li.innerHTML = `
+        <span class = "contato-nome">${inputName.value}</span> 
+        <span class = "contato-nome">${inputEmail.value}</span>
+        <span class = "contato-nome">${inputTel.value}</span>
+        `;
+
+        //Adiciona um o elemento li dentro do ul na lista HTML
+        lista.appendChild(li);
+
+        //resetar o formulario
+        form.reset();
+
+        //oculta a tag li do HTML
+        // listaMsg.style.display = "none";
+        listaMsg.remove();
+
+
     }
-
-    //Criar li com javascript
-    const li = document.createElement("li");
-    li.innerHTML = `
-    <span class = "contato-nome">${inputName.value}</span> 
-    <span class = "contato-nome">${inputEmail.value}</span>
-    <span class = "contato-nome">${inputTel.value}</span>
-    `;
-
-    //Adiciona um o elemento li dentro do ul na lista html
-    lista.appendChild(li);
-
-    //resetar o formulario
-    form.reset();
-
 })
