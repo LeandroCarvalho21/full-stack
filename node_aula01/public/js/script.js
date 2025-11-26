134bb606a151e3be401bff69c0ec75cb989f6333
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-11-26 14:08:20 -0300

    Corrigindo erro no menu

--- node_aula01/public/js/script.js
+++ node_aula01/public/js/script.js
@@ -1,19 +1,27 @@
 let menu = document.querySelector("#menu");
 let close = document.querySelector("#close");
 let ul = document.querySelector("#links");
+let logo = document.querySelector(".logo");
+let title = document.querySelector(".title");
+let nav = document.getElementsByTagName('nav')[0];
 
 menu.addEventListener('click', () => {
-    if (menu == menu) {
-        close.style.display = "block";
-        menu.style.display = "none";
-        ul.style.display = "block";
-    }
+    close.style.display = "block";
+    menu.style.display = "none";
+    ul.style.display = "block";
+    logo.style.display = "none";
+    title.style.display = "none";
+    nav.style.width = "100%";
+    nav.style.color = "red";
+    console.log(nav);
 })
 
 close.addEventListener('click', () => {
-    if (close == close) {
-        close.style.display = "none";
-        menu.style.display = "block";
-         ul.style.display = "none";
-    }
+    close.style.display = "none";
+    menu.style.display = "block";
+    ul.style.display = "none";
+    logo.style.display = "block";
+    title.style.display = "block";
+    nav.style.width = "0%";
+
 })
