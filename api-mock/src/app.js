ad889dcf62e3771ff67fcc95c8b5f47ef87c2184
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-12-08 16:18:44 -0300

    adicionando rota delete

--- api-mock/src/app.js
+++ api-mock/src/app.js
@@ -6,8 +6,38 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Olá node.js");
+    res.send("Copa do Mundo");
 });
 
+//Buscar todas as seleções
+app.get("/selecoes", (req, res) => {
+    const sql = "select*from selecoes";
+    conexao.query(sql, (erro, result) => {
+        res.json(result);
+    })
+})
+
+//Buscando por ID
+app.get("/selecoes/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = "select * from selecoes where id=?;";
+    conexao.query(sql, id, (erro, result) => {
+        res.json(result[0]);
+    });
+})
+
+//Deletar por ID
+app.delete("/selecoes/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = "delete from selecoes where id=?;";
+    conexao.query(sql, id, (erro, result) => {
+        if (result.affectedRows === 0) {
+            return res.status(404).send(`Registro não encontrado`)
+            // return (`erro id não encontrado ${id}`);
+        } else {
+            res.status(200).send(`Time deletado`);
+        }
+    });
+})
 
 export default app;
