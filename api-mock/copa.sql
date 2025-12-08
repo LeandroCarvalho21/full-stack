38cad2eb0d995ed7097f8dc2853408f473411005
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-12-08 14:26:55 -0300

    salvando o script copa.sql

---
+++ api-mock/copa.sql
@@ -0,0 +1,24 @@
+-- Criar DB 
+create database	copa_mundo;
+
+-- usar o DB copa_mundo
+use copa_mundo;
+
+-- cria a tabela seleções com 3 colunas
+create table selecoes (
+id int auto_increment primary key,
+selecao varchar (50) not null,
+grupo char (1) not null
+);
+
+-- seleciona a tabela selecoes
+select*from selecoes;
+
+insert into selecoes(selecao,grupo)value
+('Brasil','A'),
+('França','B'),
+('Ilhas de salomão','C'),
+('Nigéria','D'),
+('Nova Zelândia','E'),
+('Japão','C');
+
