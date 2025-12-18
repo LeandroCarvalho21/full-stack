0b6fac231f6efbf8a1e1a3feb985da928d9f6a81
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-12-18 16:19:12 -0300

    Criando nova pasta public

--- api-mock/src/app.js
+++ api-mock/src/app.js
@@ -9,11 +9,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+
 app.get("/", (req, res) => {
     res.send("Olá Copa do Mundo!");
 });
 
 //indica a rota da seleções
 app.use(seleçoesRoutes);
+
+//rota 404
+app.use((req,res)=>{
+    res.status(404).json({mensagem:"Rota não encostrada"}); 
+})
 
 export default app;
